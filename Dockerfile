d03ac025f5cc8004f9c819afdb6a8873474b6e4c
romanChub <roman.chubinskyi@vitechteam.com>
2023-02-17 11:29:03 +0200

    create project

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM python:3.6
+
+RUN mkdir -p /usr/src/app/
+
+WORKDIR /usr/src/app
+
+COPY . /usr/src/app/
+
+#RUN /usr/local/bin/python -m pip install --upgrade pip
+RUN pip install --no-cache-dir -r requirements.txt
+
+EXPOSE 8080
+
+RUN chmod a+x ./run.sh
+
+CMD ["python", "src/app.py"]
---
+++ src/app.py
@@ -0,0 +1,15 @@
+import os
+import datetime
+from flask import Flask
+
+app = Flask(__name__)
+
+BASE_FOLDER = os.path.dirname(os.path.abspath(__file__))
+
+
+@app.route('/')
+def hello_world():
+    return "%s - %s" % ("Hello World", datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S"))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
